bf79c5498384da1db342d2df7625fe76c2ee06e5
Moritz R <c0derMo@users.noreply.github.com>
2025-07-10 00:02:54 +0200

    [client] update bracket game view

--- client/src/components/StageGame.vue
+++ client/src/components/StageGame.vue
@@ -10,14 +10,32 @@
         ></div>
 
         <div class="column box">
-            <span class="text-weight-thin italic">
+            <div
+                class="text-weight-thin text-italic q-mx-auto q-mt-xs small-text"
+            >
                 Match {{ game.matchNumber }}
-            </span>
+            </div>
 
             <div class="row justify-center q-gutter-sm">
-                <div class="score">{{ game.result?.scores[0] }}</div>
+                <div
+                    class="score text-right"
+                    :class="{
+                        win: isWinner(0),
+                        loss: hasResult && !isWinner(0),
+                    }"
+                >
+                    {{ game.result?.scores[0] }}
+                </div>
                 <span>:</span>
-                <div class="score">{{ game.result?.scores[1] }}</div>
+                <div
+                    class="score"
+                    :class="{
+                        win: isWinner(1),
+                        loss: hasResult && !isWinner(1),
+                    }"
+                >
+                    {{ game.result?.scores[1] }}
+                </div>
             </div>
 
             <div class="row justify-center q-gutter-md">
@@ -31,7 +49,7 @@
                             )
                         }}
                     </template>
-                    <span v-else class="text-italic">
+                    <span v-else class="text-italic small-text">
                         {{ game.templateParticipantNames[0] }}
                     </span>
                 </div>
@@ -45,7 +63,7 @@
                             )
                         }}
                     </template>
-                    <span v-else class="text-italic">
+                    <span v-else class="text-italic small-text">
                         {{ game.templateParticipantNames[1] }}
                     </span>
                 </div>
@@ -62,14 +80,23 @@
 <script setup lang="ts">
 import type { IStageGame } from "@shared/interfaces/IStageGame";
 import { useParticipantNames } from "../composables/participantNames";
+import { computed } from "vue";
 
 const participantNames = useParticipantNames();
 
-defineProps<{
+const props = defineProps<{
     game: IStageGame;
     hasSucceedingMatches: boolean;
     stageNumber: number;
 }>();
+
+function isWinner(index: number): boolean {
+    return props.game.result?.ranking[0] === index;
+}
+
+const hasResult = computed<boolean>(() => {
+    return props.game.result != null;
+});
 </script>
 
 <style scoped>
@@ -78,14 +105,28 @@ defineProps<{
     border: 1px solid white;
     border-radius: 0.5rem;
 }
-.italic {
-    font-style: italic;
+
+.small-text {
+    font-size: 12px;
+    color: #999999;
 }
 
 .score {
-    background-color: white;
-    color: black;
     min-width: 1rem;
+    border-radius: 25%;
+    padding-left: 3px;
+    padding-right: 3px;
+    background-color: #333333;
+}
+
+.score.win {
+    background-color: #c9a227;
+    color: black;
+}
+
+.score.loss {
+    background-color: #aaaaaa;
+    color: black;
 }
 
 .connecting-tab {
